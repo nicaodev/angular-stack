ddf176a7a9caedfa474cc6e91f81c664c8145b45
nicaodev <alexandre.nicos@gmail.com>
2022-11-29 19:42:55 -0300

    ajuste show component

--- angular-stack/src/app/app.component.ts
+++ angular-stack/src/app/app.component.ts
@@ -3,30 +3,31 @@ import { AfterContentChecked, AfterContentInit, AfterViewInit, Component, DoChec
 @Component({
   selector: 'app-root',
   template: `
-  <app-title *ngIf="destruir" title="Mudando via app.component"></app-title>
+  <app-title *ngIf="show" title="Mudando via app.component"></app-title>
   <button (click)="adicionar()">Add</button>
   <br>
   {{valor}}
   <br>
   <br>
-  <button (click)="destruirComponent()">Destruir Componente</button>
+  <button (click)="boolComponent()">Bool Componente</button>
   <router-outlet></router-outlet>
   `
 })
 export class AppComponent implements
-OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit {
+  OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit {
 
   constructor() { }
 
 
   public valor: number = 1;
-  public destruir: boolean = true;
+  public show: boolean = true;
 
   public adicionar(): number {
     return this.valor += 1;
   }
-  public destruirComponent(): void {
-    this.destruir = false;
+  public boolComponent(): void {
+    this.show = !this.show;
+    // Só para mostrar a ativação do ciclo de vida do componente.
   }
 
   ngOnInit(): void {
