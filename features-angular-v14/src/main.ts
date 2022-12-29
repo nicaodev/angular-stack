bea5eeb62645bba1b725e7e265466b45506736d0
nicaodev <alexandre.nicos@gmail.com>
2022-12-29 20:04:36 -0300

    config para uso do standalone

--- features-angular-v14/src/main.ts
+++ features-angular-v14/src/main.ts
@@ -1,7 +1,22 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
 
+// antes
+// import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
+
+import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { importProvidersFrom } from '@angular/core';
+import { AppRoutingModule } from './app/app-routing.module';
+
+//depois
+// Muda configurações para funcionar com standalone component
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(AppRoutingModule) // Implementa rotas para dentro do standalone component
+  ]
+}).catch(err => console.error(err));
+
