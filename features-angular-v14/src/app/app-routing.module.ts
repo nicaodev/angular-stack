f51ee80c52f0203ad36e9f27673b64801732a76d
nicaodev <alexandre.nicos@gmail.com>
2022-12-29 20:14:45 -0300

    standalone component

--- features-angular-v14/src/app/app-routing.module.ts
+++ features-angular-v14/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './core/header/header.component';
+import { SobreComponent } from './core/sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '', component: HeaderComponent
+},
+{
+  path: 'sobre', component: SobreComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
